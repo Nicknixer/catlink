8cf4b9b5e667079bac1f513f7a97010a7917e973
Nick Sergeychuk <nicknixer@gmail.com>
2017-01-22 16:34:10 +0300

    Add action show site

---
+++ src/AppBundle/Controller/SiteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class SiteController extends Controller
+{
+
+    /**
+     *
+     *
+     * @Route("/site/{id}", name="showSite")
+     *
+     */
+    public function showSiteAction($id)
+    {
+        $site = $this->getDoctrine()
+            ->getRepository('AppBundle:Site')
+            ->findOneBy([
+                'id' => $id,
+                'isModerated' => 1
+            ]);
+
+        if (!$site) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('AppBundle:Default:showsite.html.twig', [
+            'site' => $site
+        ]);
+    }
+}
