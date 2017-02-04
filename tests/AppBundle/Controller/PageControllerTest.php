a052cefb345ee3b3d66c3a6dc15062ae674fa975
Nick Sergeychuk <nicknixer@gmail.com>
2017-02-04 17:07:11 +0300

    test: add test for feedback page and form

--- tests/AppBundle/Controller/PageControllerTest.php
+++ tests/AppBundle/Controller/PageControllerTest.php
@@ -34,4 +34,33 @@ class PageControllerTest extends WebTestCase
             $crawler->filter('html:contains("О сайте")')->count()
         );
     }
+
+    // Testing feedback page
+    public function testFeedback()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/feedback');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Обратная связь")')->count()
+        );
+
+        // Test form
+        $form = $crawler->selectButton('form[send]')->form();
+        $crawler = $client->submit($form, [
+            'form[name]' => 'Nick',
+            'form[email]' => 'test@test.ru',
+            'form[message]' => 'Test message',
+        ]);
+
+        // After sending site must show sender's name in page
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Nick")')->count()
+        );
+    }
 }
