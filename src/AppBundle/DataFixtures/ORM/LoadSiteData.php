12a6ece2719b87c663a74b719df612a430aae882
Nick Sergeychuk <nicknixer@gmail.com>
2017-02-07 22:13:53 +0300

    add load fixture for first site

--- src/AppBundle/DataFixtures/ORM/LoadSiteData.php
+++ src/AppBundle/DataFixtures/ORM/LoadSiteData.php
@@ -11,6 +11,31 @@ class LoadSiteData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $sitesData[] = [
+            'title' => 'Games for mobile',
+            'url' => 'https://games.com',
+            'email' => 'test@games.com',
+            'shortDescription' => 'Site with games for mobile',
+            'description' => 'The Best Free Mobile Games! Play arcade games, puzzle games, racing games, adventure games & more online now!',
+            'keywords' => 'games, mobile',
+            'isModerated' => 1,
+            'passes' => 0
+        ];
+
+        foreach ($sitesData as $siteData) {
+            $site = new Site();
+            $site->setTitle($siteData['title']);
+            $site->setUrl($siteData['url']);
+            $site->setEmail($siteData['email']);
+            $site->setShortDescription($siteData['shortDescription']);
+            $site->setDescription($siteData['description']);
+            $site->setKeywords($siteData['keywords']);
+            $site->setIsModerated($siteData['isModerated']);
+            $site->setPasses($siteData['passes']);
+            $site->setCategory($this->getReference('Downloads'));
+            $manager->persist($site);
+        }
+        $manager->flush();
 
     }
 
