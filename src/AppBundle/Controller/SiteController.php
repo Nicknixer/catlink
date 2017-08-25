aa6d948174b6ef528695d52365ebba59a718f0b5
Nick Sergeychuk <nicknixer@gmail.com>
2017-08-25 17:50:24 +0300

    refac: simplify showSiteAction

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -5,13 +5,6 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Site;
 use AppBundle\Form\CommentType;
@@ -24,22 +17,14 @@ class SiteController extends Controller
      *
      * @Route("/site{trailingSlash}", requirements={"trailingSlash" = "[/]{0,1}"})
      * @Route("/site/{id}", name="showSite")
-     *
+     * @param Site $site
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function showSiteAction($id = -1)
+    public function showSiteAction(Site $site)
     {
-        $site = $this->getDoctrine()
-            ->getRepository('AppBundle:Site')
-            ->findOneBy([
-                'id' => $id,
-                'isModerated' => 1
-            ]);
-
-        if (!$site) {
+        if ($site->getIsModerated() != 1) {
             return $this->redirectToRoute('homepage');
         }
-
-
 
         return $this->render('AppBundle:Site:showsite.html.twig', [
             'site' => $site,
