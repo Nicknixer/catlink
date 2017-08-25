080219a861977168b6b4d3cc40154fadac6392d2
Nick Sergeychuk <nicknixer@gmail.com>
2017-08-25 19:14:52 +0300

    admin: added Category managing

---
+++ src/AppBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', 'text');
+        $formMapper->add('description', 'textarea');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+        $listMapper->addIdentifier('description');
+    }
+}
